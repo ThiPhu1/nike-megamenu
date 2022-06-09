c64067ec3657cd774e478b8a178b5c34b21b8465
ThiPhu <thingphu1012@gmail.com>
2022-06-10 06:15:24 +0700

    fix: overflow sidebar

--- components/MegaMenu/Sidebar/subMenu.js
+++ components/MegaMenu/Sidebar/subMenu.js
@@ -22,8 +22,8 @@ export default function SubMenu(props) {
     }
 
     const onBackBtnClick = () => {
-        setSubMenuActive(false);
-        setMainMenuActive(true);
+        setSubMenuActive("");
+        setMainMenuActive("");
     }
 
     return (
